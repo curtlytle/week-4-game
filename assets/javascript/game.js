b850e60bad0403a0a25cde24f63415daead05051
Curtis Lytle <curtlytle@gmail.com>
2017-10-12 13:11:21 -0600

    added animate

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
+    $.fn.extend({
+        animateCss: function (animationName) {
+            var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+            this.addClass('animated ' + animationName).one(animationEnd, function() {
+                $(this).removeClass('animated ' + animationName);
+            });
+            return this;
+        }
+    });
+
     var tanks = [
         {tankId: "tank1", tankName: "American M26", hp: 75, tankImage: "assets/images/americanTank.png", friend: -1},
         {tankId: "tank2", tankName: "German Panther", hp: 80, tankImage: "assets/images/germanTank1.png", friend: -1},
@@ -8,6 +18,8 @@ $(document).ready(function () {
     ];
 
     var friendCnt = 0;
+    var instrution1 = $(".instructions1");
+    var instruction2 = $(".instructions2");
 
     for (var i = 0; i < tanks.length; i++) {
         var tank = tanks[i];
@@ -20,13 +32,16 @@ $(document).ready(function () {
         $(this).empty();
         var id = $(this).attr('id');
         var tank = getTankElement(id);
+        var $tdiv = getTankDiv(tank);
 
         if (friendCnt === 0) {
-            $("#friendlist").prepend(getTankDiv(tank));
+            $("#friendlist").prepend($tdiv);
             tank.friend = 1;
             friendCnt++;
+            instruction2.html("Choose your Enemy");
+            $tdiv.animateCss("shake");
         } else {
-            $("#enemylist").prepend(getTankDiv(tank));
+            $("#enemylist").prepend($tdiv);
             tank.friend = 0;
         }
     });
@@ -56,7 +71,11 @@ $(document).ready(function () {
         var healthhtml = "<p>" + tank.hp + "</p>";
         $scorediv.html(healthhtml);
 
+        $div.animateCss("shake");
+
+
         return $div;
     }
 
 });
+
